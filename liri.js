88eccf1e8ed1081838a58a58ba25cf67ee8205fe
tommyngre <tommyn.gre@gmail.com>
2018-04-18 18:23:13 -0400

    notes

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+//import keys.js & set to var
+///access keys via
+///var spotify = new Spotify(keys.spotify);
+///var client = new Twitter(keys.twitter);
+
+//liri should take these commands
+///my-tweets
+///spotify-this-song
+///movie-this
+///do-what-it-says
