8402da9e8ac4dfaf81d2cfc623e11b3e70e3a3f7
unknown <tommyn.gre@gmail.com>
2018-04-21 21:27:58 -0400

    omdb

--- liri.js
+++ liri.js
@@ -7,26 +7,34 @@ var spotify = new Spotify(keys.spotify);
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 
+var request = require("request");
+
 let command = process.argv[2];
 let commandParams = process.argv;
 commandParams.splice(0, 3);
 let paramStr = commandParams.join(' ');
 
-console.log("paramStr ", paramStr);
-
 function printTweet(tweet) {
   console.log("/================================================================/")
-  console.log(` ${tweet.user.name.toUpperCase()} tweeted on ${tweet.created_at}:`) 
-  console.log(` "${tweet.text}"`) 
+  console.log(` ${tweet.user.name.toUpperCase()} tweeted on ${tweet.created_at}:`)
+  console.log(` "${tweet.text}"`)
   console.log("/================================================================/")
 
 }
 
 function printSong(song) {
   console.log("/================================================================/")
-  console.log(` "${song.name}" by ${song.artist}`) 
+  console.log(` "${song.name}" by ${song.artist}`)
   console.log(` From: ${song.album}`)
   console.log(` Listen @ ${song.link}`)
+  console.log("/================================================================/")
+}
+
+function printMovie(movie) {
+  console.log("/================================================================/")
+  //console.log(` "${song.name}" by ${song.artist}`)
+  //console.log(` From: ${song.album}`)
+  //console.log(` Listen @ ${song.link}`)
   console.log("/================================================================/")
 }
 
@@ -74,6 +82,7 @@ function handler() {
               link: track.external_urls.spotify,
               album: track.album.name
             }
+
           printSong(song);
         }
       });
@@ -81,6 +90,34 @@ function handler() {
 
     //omdb==================================================
     case "movie-this":
+      //default to Mr. Nobody
+      if (paramStr = '') {
+        paramStr = "Mr. Nobody";
+      }
+
+      //request
+      request("https://www.omdbapi.com/?t=" + paramStr + "&plot=short&apikey=trilogy", function (error, response, body) {
+
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(body);
+
+          let movie = {
+            title: '',
+            year: '',
+            imdb: '',
+            rottenTomatoes: '',
+            country: '',
+            language: '',
+            plot: '',
+            actors: ''
+          }
+
+          printMovie(movie)
+        }
+
+      });
 
       break;
 
@@ -93,12 +130,5 @@ function handler() {
       console.log("No command... Sad!");
   }
 }
-
-
-//liri should take these commands
-///my-tweets
-///spotify-this-song
-///movie-this
-///do-what-it-says
 
 handler(command);
